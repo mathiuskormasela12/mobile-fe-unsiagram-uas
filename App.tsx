65ae3db53ee27a844576728f7084baaaedb0bbb9
Mathius Kormasela <mathiuskormasela12@gmail.com>
2024-02-12 01:41:38 +0700

    update: setup jail-monkey.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
 import RootScreen from '@src/RootScreen'
 import { ReduxWrapper } from '@src/components'
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import JailMonkey from 'jail-monkey'
+import { Alert, BackHandler } from 'react-native'
 
 export default function App (): React.ReactNode {
+  const handleExitApp = (): void => {
+    BackHandler.exitApp()
+  }
+
+  useEffect(() => {
+    if (JailMonkey.isJailBroken()) {
+      Alert.alert('Warning', "Careful, we've detected that your device has been jailbroken/rooted", [
+        {
+          text: 'Ok',
+          style: 'destructive',
+          onPress: handleExitApp
+        }
+      ])
+    }
+  }, [])
+
   return (
    <Fragment>
     <ReduxWrapper>
