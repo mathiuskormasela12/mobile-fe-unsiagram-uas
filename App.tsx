38dac00a54dc024e7723eb0f943fb7a4431aae15
Mathius Kormasela <mathiuskormasela12@gmail.com>
2024-02-12 00:46:35 +0700

    update: setup redux.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import RootScreen from '@src/RootScreen'
+import { ReduxWrapper } from '@src/components'
 import React, { Fragment } from 'react'
 
 export default function App (): React.ReactNode {
   return (
    <Fragment>
-    <RootScreen />
+    <ReduxWrapper>
+      <RootScreen />
+    </ReduxWrapper>
    </Fragment>
   )
 }
--- src/RootScreen.tsx
+++ src/RootScreen.tsx
@@ -1,8 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useCallback } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+import { type AppDispatch, type RootState } from './redux/store'
+import { increment } from './redux/slices/counterSlice'
+import { incrementTemp } from './redux/slices/counterTempSlice'
 
 const RootScreen: React.FC = () => {
+  const disptach = useDispatch<AppDispatch>()
+  const like = useSelector((states: RootState) => states.counterReducer.count)
+  const dislike = useSelector((states: RootState) => states.counterTempReducer.count)
+
+  const handleLike = useCallback(() => {
+    disptach(increment({
+      count: 2
+    }))
+  }, [])
+
+  const handleDislike = useCallback(() => {
+    disptach(incrementTemp({
+      count: 2
+    }))
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Welcome to React Native + Expo</Text>
@@ -11,6 +31,8 @@ const RootScreen: React.FC = () => {
       <Text>Build Number : {process.env.EXPO_PUBLIC_BUILD_NUMBER}</Text>
       <Text>API URL : {process.env.EXPO_PUBLIC_API_URL}</Text>
       <Text>APP URL : {process.env.EXPO_PUBLIC_APP_URL}</Text>
+      <Button title={`Like ${like}`} onPress={handleLike} />
+      <Button title={`Dislike ${dislike}`} onPress={handleDislike} />
       <StatusBar style="auto" />
     </View>
   )
