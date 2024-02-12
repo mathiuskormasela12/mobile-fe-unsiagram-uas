d950335e0a151c69055b9506424314b9d8f0501d
Mathius Kormasela <mathiuskormasela12@gmail.com>
2024-02-13 00:49:54 +0700

    feat: login page.

--- src/screens/Auth/LoginScreen/index.tsx
+++ src/screens/Auth/LoginScreen/index.tsx
@@ -1,12 +1,120 @@
-import { SafeAreaView } from '@src/components'
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, Image, View, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, ScrollView } from 'react-native'
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons'
 import styles from './styles'
+import { ColorNeutral, Layout } from '@src/themes'
+import { Input, Label, Button, SafeAreaView, HeaderAuth } from '@src/components'
+import { useLoginScreen } from '@src/hooks'
+import { s } from '@src/helpers'
+import { Controller } from 'react-hook-form'
 
 const LoginScreen: React.FC = () => {
+  const {
+    handleNavigate,
+    control,
+    errors,
+    handleSubmit,
+    handleLogin
+  } = useLoginScreen()
+
   return (
     <SafeAreaView style={styles.page}>
-      <Text style={styles.title}>Hello LoginScreen</Text>
+      <HeaderAuth />
+      <View style={[Layout.container, Layout.fullPage]}>
+        <KeyboardAvoidingView
+          behavior={'padding'}
+          style={styles.main}
+        >
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            alwaysBounceVertical={false}
+            contentContainerStyle={styles.scrollView}
+          >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+              <View style={Layout.fullPage}>
+                <View style={styles.header}>
+                  <Image
+                    source={require('../../../../assets/img/unsia.png')}
+                    style={styles.img}
+                  />
+                  <Text style={styles.title}>
+                    Welcome back to Unsiagram
+                  </Text>
+                </View>
+                <View style={styles.form}>
+                  <View style={styles.control}>
+                    <Label>Email</Label>
+                    <Controller
+                      control={control}
+                      name='email'
+                      render={({ field: { onBlur, onChange } }) => {
+                        return (
+                          <Input
+                            leftIcon={
+                              <Ionicons
+                                name='mail-sharp'
+                                color={ColorNeutral.neutral50}
+                                size={s(13)}
+                              />
+                            }
+                            message={errors?.email?.message ?? undefined}
+                            placeholder='Type your last name here...'
+                            placeholderTextColor={ColorNeutral.neutral30}
+                            onChangeText={onChange}
+                            onBlur={onBlur}
+                          />
+                        )
+                      }}
+                    />
+                  </View>
+                  <View style={styles.control}>
+                    <Label>Password</Label>
+                    <Controller
+                      control={control}
+                      name='password'
+                      render={({ field: { onBlur, onChange } }) => {
+                        return (
+                          <Input
+                            leftIcon={
+                              <FontAwesome5
+                                name='lock'
+                                color={ColorNeutral.neutral50}
+                                size={s(13)}
+                              />
+                            }
+                            onChangeText={onChange}
+                            onBlur={onBlur}
+                            secureTextEntry={true}
+                            message={errors?.password?.message ?? undefined}
+                            placeholder='Type your Password here...'
+                            placeholderTextColor={ColorNeutral.neutral30}
+                          />
+                        )
+                      }}
+                    />
+                  </View>
+                  <View style={styles.control}>
+                    <Text
+                      onPress={handleNavigate.bind(this, 'RegisterScreen')}
+                      style={[Layout.textRight, styles.rightText]}
+                    >
+                      {"Don't have any account ?"}
+                    </Text>
+                  </View>
+                  <View style={styles.control}>
+                    <Button
+                      variant='primary'
+                      onPress={handleSubmit(handleLogin)}
+                    >
+                      Login
+                    </Button>
+                  </View>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </View>
     </SafeAreaView>
   )
 }
--- src/screens/Auth/LoginScreen/styles.ts
+++ src/screens/Auth/LoginScreen/styles.ts
@@ -1,16 +1,50 @@
 import { s } from '@src/helpers'
-import { ColorNeutral, Fonts } from '@src/themes'
-import { StyleSheet } from 'react-native'
+import { ColorNeutral, ColorPrimary, Fonts } from '@src/themes'
+import { Platform, StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
+  header: {
+    alignItems: 'center',
+    marginTop: Platform.select({ android: s(15), ios: 0 })
+  },
+  img: {
+    width: s(140),
+    height: s(125),
+    resizeMode: 'contain'
+  },
+  main: {
+    justifyContent: 'center',
+    flex: 1
+  },
   title: {
-    fontSize: s(16),
-    color: 'red',
-    fontFamily: Fonts.base
+    fontSize: s(24),
+    color: ColorNeutral.neutral90,
+    fontFamily: Fonts.semiBold,
+    textAlign: 'center',
+    marginTop: s(7),
+    fontWeight: 'bold',
+    width: s(320),
+    lineHeight: s(32)
   },
   page: {
     flex: 1,
-    backgroundColor: ColorNeutral.neutral0
+    backgroundColor: ColorNeutral.neutral0,
+    paddingBottom: s(15)
+  },
+  form: {
+    marginTop: s(35)
+  },
+  control: {
+    marginBottom: s(18)
+  },
+  rightText: {
+    fontFamily: Fonts.base,
+    fontSize: s(14),
+    color: ColorPrimary.primary50,
+    marginBottom: s(10)
+  },
+  scrollView: {
+    flexGrow: 1
   }
 })
 
